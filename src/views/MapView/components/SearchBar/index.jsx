efb91497563156ed6a1bf88469498eef07e15220
Piotr <niemczykpiotr13@gmail.com>
2020-06-24 01:25:40 +0200

    Add screen keyboard

--- src/views/MapView/components/SearchBar/index.jsx
+++ src/views/MapView/components/SearchBar/index.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import Keyboard from 'react-simple-keyboard';
 import { ReactComponent as Search } from 'assets/search-solid.svg';
 import classNames from 'classnames';
 
+import 'react-simple-keyboard/build/css/index.css';
+import './keyboard.css';
 import * as styles from './styles.module.scss';
 
 export default class SearchBar extends Component {
@@ -10,26 +13,63 @@ export default class SearchBar extends Component {
 
     this.state = {
       isFocused: false,
+      openKeyboard: false,
+      searchValue: '',
     };
   }
 
+  componentDidMount() {
+    document.addEventListener('click', this.clickedOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.clickedOutside);
+  }
+
+  clickedOutside = event => {
+    const keyboard = document.getElementById('keyboard_wrapper');
+    const isClickedKeyboard = keyboard.contains(event.target);
+    const { openKeyboard } = this.state;
+
+    if (openKeyboard && !isClickedKeyboard) {
+      this.toggleKeyboard(false);
+    }
+  };
+
   toggleFocus = () => this.setState({ isFocused: !this.state.isFocused });
 
+  onChange = value => this.setState({ searchValue: value });
+
+  handleChange = event => this.setState({ searchValue: event.target.value });
+
+  toggleKeyboard = booleanValue => this.setState({ openKeyboard: booleanValue });
+
   render() {
-    const { isFocused } = this.state;
+    const { isFocused, openKeyboard, searchValue } = this.state;
 
     return (
-      <div className={classNames(styles.searchbarWrapper, isFocused && styles.focused)}>
-        <div className={styles.searchbar}>
-          {!isFocused && <Search onClick={this.toggleFocus} />}
-          {isFocused && (
-            <div className={styles.inputWrapper}>
-              <input name="search" type="text" />
-              <Search onClick={this.toggleFocus} />
-            </div>
-          )}
+      <React.Fragment>
+        <div className={classNames(styles.searchbarWrapper, isFocused && styles.focused)}>
+          <div className={styles.searchbar}>
+            {!isFocused && <Search onClick={this.toggleFocus} />}
+            {isFocused && (
+              <div className={styles.inputWrapper}>
+                <input
+                  name="search"
+                  type="text"
+                  value={searchValue}
+                  onFocus={() => this.toggleKeyboard(true)}
+                  onChange={this.handleChange}
+                />
+                <Search onClick={this.toggleFocus} />
+              </div>
+            )}
+          </div>
         </div>
-      </div>
+        <div id="keyboard_wrapper" className={classNames(styles.keyboardWrapper, !openKeyboard && styles.hidden)}>
+          <Keyboard inputName={'search'} className={styles.keyboard} onChange={this.onChange} />
+        </div>
+      </React.Fragment>
     );
   }
 }
